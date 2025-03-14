d8c6aa645fdc8ce0c52a7acc80cb515a733e19f4
Ella <EllaP.jobs@gmail.com>
2025-03-14 22:12:38 +0100

    added a safe testing environment, a lot easier said than done, Nginx syntax is just, there's none available, so I had to use its JS module, yet still had to bypass Nginx being plain stupid in every way it can, but I finally did it, I am able to not leak the IP addresses of my whitelist, like excuse me? This seems like an obvious feature someone might want? Nginx says 'fuck u :relieved:'(auto push :heart:)

---
+++ backend/webserver/ip_whitelist.js
@@ -0,0 +1,20 @@
+// Redirect whitelisted IPs to the testing environment, blocking everyone else
+function check_ip(r)
+{
+	// Setup the whitelist from our environment variables
+	var whitelist =
+	[
+		process.env.SCHOOL_ETHERNET,
+		process.env.SCHOOL_WIFI,
+		process.env.ELLA
+	]
+
+	// Check if client's IP is within the whitelist, block if not
+	if (!whitelist.includes(r.remoteAddress))
+		return r.return(403);
+	r.internalRedirect("/test_environment/");
+	// Otherwise, redirect to the internally served testing environment
+}
+
+// Export the 'check_ip' function
+export default { check_ip }
