c0ff226b4b97a1c73a879f0560658d0247f7b402
P-Claus <pieter.claus1@gmail.com>
2025-05-02 17:33:32 +0200

    started work on teh registerform so the user can register an account. Still need the actual request to the server

--- frontend/test_environment/dist/router.js
+++ frontend/test_environment/dist/router.js
@@ -1,10 +1,13 @@
 import { renderHomePage } from "./src/pages/HomePage.js";
 import { renderCookiePolicy } from "./src/pages/CookiePolicy.js";
 import { renderLogin } from "./src/pages/Login.js";
+import { renderRegister } from "./src/pages/Register.js";
+import { attachRegisterFormListener } from './src/pages/Register.js';
 const routes = {
     "/safe": renderHomePage,
     "/safe/login": renderLogin,
     "/safe/cookie-policy": renderCookiePolicy,
+    "/safe/register": renderRegister,
 };
 export const navigateTo = (url) => {
     history.pushState({}, "", url);
@@ -19,17 +22,30 @@ export const router = () => {
         console.error("❌ ERROR: <div id='app'> NOT FOUND!");
         return;
     }
-    setTimeout(attachMenuListener, 0);
+    //setTimeout(attachMenuListener, 0);
+    //setTimeout(attachRegisterFormListener, 0);
     const page = routes[path] ? routes[path]() : "<h1>404 - Page Not Found</h1>";
     app.innerHTML = page;
+    attachMenuListener();
+    if (path === "/safe/register")
+        attachRegisterFormListener();
 };
 // Handle browser back/forward navigation
 window.addEventListener("popstate", router);
 const attachMenuListener = () => {
     const mobileMenuButton = document.querySelector(".mobile-menu-button");
     const mobileMenu = document.querySelector(".navigation-menu");
+    console.log("The listener function for the menu runs");
     mobileMenuButton.addEventListener("click", () => {
         if (mobileMenu)
             mobileMenu.classList.toggle("hidden");
     });
 };
+//const attachRegisterFormListener = () => {
+//	const registerForm = document.querySelector("#registerForm");
+//	console.log("The listener function for the form runs");
+//	registerForm.addEventListener('submit', (event) => {
+//		event.preventDefault();
+//		console.log("The event listener for the form is added");
+//	});
+//};
---
+++ frontend/test_environment/src/pages/Register.ts
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import { renderNavBar } from '../components/NavBar.js'
+import { renderFooter } from '../components/Footer.js'
+
+
+export const attachRegisterFormListener = () => {
+	const registerForm = document.querySelector('#registerForm');
+	console.log("The atachRegisterFormListener runs");
+	registerForm.addEventListener('submit', (event) => {
+		event.preventDefault();
+		console.log("The event listener for the form is added");
+		console.log("The event is: ", event.target);
+	});
+};
+
+//setTimeout(attachRegisterFormListener, 0);
+
+//export const sendDataToBackend = async (event) => {
+//	event.preventDefault();
+//	console.log("The event is: ", event);
+// 	//const { data } = await axios.post('http://localhost:1919/login', {
+// 	//		email, password, 
+// 	//	}, {
+// 	//		headers: {
+// 	//			'Content-Type': 'application/json'
+// 	//		}
+// 	//	}
+// 	//)
+// 	//console.log("The data is: ", data);
+// 	//return data;
+// }
+
+export const renderRegister = (): string => {
+	return `
+  	${renderNavBar()}
+	  <section class="bg-hero-pattern text-white bg-cover bg-top w-full">
+		<div class="container px-5 md:px-10 h-screen flex items-center justify-center">
+			<div class="px-6 flex flex-col items-center bg-primary-background rounded-xl w-96">
+					<h2 class="mb-5 mt-7">Register</h2>
+					<form class="flex flex-col gap-2 w-72" id="registerForm" >
+						<label for="email" class="text-base">Email</label>
+						<input class="h-10 rounded text-black" type="email" id="email" name="email">
+						<label for="password" class="text-base">Password</label>
+						<input class="h-10 rounded text-black" type="password" id="password" name="password">
+						<button class="h-10 w-full mt-10 text-base md:text-base text-white bg-primary my-8 py-3 px-6 rounded-md justify-center flex items-center whitespace-nowrap hover:text-primary hover:bg-white" type="submit">Register</button>
+					</form>
+					<p class="mb-10 text-base" data-link>Already have an account? Click <a class="underline text-primary pointer" href="/safe/login" data-link>here</a> to login</p>
+			</div>
+		</div>
+	  </section>
+	${renderFooter()}
+  `;
+}
