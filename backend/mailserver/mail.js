95dd30c62c3e5a3d197bd543fdb7022384b96309
Ella <EllaP.jobs@gmail.com>
2025-07-08 20:52:31 +0200

    added a bunch of makefile commands to make restarting containers more modular instead of effecting the entire stack every minor change that needs testing, updated email server to accept HTML format and added comments everywhere for documentation, also slightly tweaked logic to ensure sockets cannot remain open. next up and final thing for verification will be to create an endpoint on daverify token. after that we will be implementing 2-factor authentication(auto push :heart:)

--- backend/mailserver/mail.js
+++ backend/mailserver/mail.js
@@ -1,12 +1,18 @@
 import Fastify from 'fastify';
+import { config } from 'dotenv';
+import { sendMail } from './server.js';
+import { readFileSync } from 'fs';
 
 const app = Fastify();
+config();
 
 function sendVerification(request, response)
 {
 	const { to, token } = request.body;
-	console.log(`endpoint /verify reached with data: ${to}, ${token}`)
-	response.send({ success: true });
+
+	return sendMail(to, 'Email Verification Required for Pong Account', readFileSync('./mail.html', 'utf-8').replace(/ERROR/g, `https://${process.env.subDomain}/api/verify?token=${token}`))
+	.then(() => response.send({ success: true }))
+	.catch(error => response.status(error.status || 500).send({ success: false, error: error.message }));
 }
 
 app.post('/verify', sendVerification);
