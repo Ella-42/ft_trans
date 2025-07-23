4b888ef0a94b35cee2f6f7cbafc5bc765edc5f5a
Ella <EllaP.jobs@gmail.com>
2025-07-23 22:43:38 +0200

    now mounting database in a globally shared path, this will be more secure and ensure HMAC is shared upon regeneratio and that data is more persistent, added UI for 2FA and reworked UI of profile tab in dashboard(auto push :heart:)

--- frontend/test_environment/src/components/ProfileComponent.ts
+++ frontend/test_environment/src/components/ProfileComponent.ts
@@ -3,6 +3,7 @@ declare const axios: any;
 declare const Swal: any;
 
 export const attachUpdateProfileFormListener = async () => {
+	const twoFactorToggle = document.querySelector<HTMLInputElement>('#twoFactorToggle');
 	const updateProfileForm = document.querySelector('#updateProfileForm');
 	const showOldPasswordIcon = document.querySelector('.lucide-eye-icon');
 	const showNewPasswordIcon= document.querySelector('.lucide-eye-icon-new');
@@ -22,6 +23,27 @@ export const attachUpdateProfileFormListener = async () => {
 	const avatarLink = avatarResponse.data.avatar;
 	const avatarBlock = document.getElementById('avatarBlock');
 	avatarBlock.innerHTML = `<img src ="${avatarLink}" width="320">`
+
+	twoFactorToggle.checked = (await axios.get(`https://trans.ella-peeters.me/api/users/${response.data.id}/2fa`)).data.two_factor;
+
+	twoFactorToggle.addEventListener('change', async () => {
+		console.log('2FA state toggled');
+		const res = await fetch(`https://trans.ella-peeters.me/api/users/${response.data.id}/2fa`, {
+			method: 'POST',
+			credentials: 'include',
+		});
+
+		const json = await res.json()
+		if (json.error) {
+			twoFactorToggle.checked = !twoFactorToggle.checked;
+			Swal.fire(
+			{
+				title: 'Oops!',
+				text: json.error,
+				icon: 'error'
+			})
+		}
+	});
 
 	const deleteProfileButton = document.querySelector("#deleteProfileButton");
 	deleteProfileButton.addEventListener('click', async (e) => {
@@ -76,6 +98,18 @@ export const renderProfile = () => {
 					<div class="px-10 py-5 rounded-xl my-5 mb-10 bg-gray-900 flex flex-col justify-between ">
 						<p class="text-xl mb-8" id="">Edit your profile and account settings.</p>
 
+						<p class="text-xl mt-10 mb-4">Settings</p>
+						<p class="mt-10 mb-2">Two-Factor Authentication</p>
+						<div class="flex items-center gap-4 mb-8">
+							<label for="twoFactorToggle" class="text-base">Enable 2FA</label>
+								<label class="relative inline-flex items-center cursor-pointer">
+								<input type="checkbox" id="twoFactorToggle" class="sr-only peer">
+								<div class="w-11 h-6 bg-gray-400 rounded-full peer-checked:bg-purple-600 transition"></div>
+								<div class="absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition peer-checked:translate-x-5"></div>
+							</label>
+						</div>
+
+						<p class="text-xl mt-10 mb-4">Update profile</p>
 						<div class="grid grid-cols-1 items-center">
 							<form id="updateProfileForm" class="grid grid-cols-1 md:grid-cols-2 md:gap-3 item-center">
 								<div class="flex items-center flex-col mt-8 pb-8 order-2 md:mt-4 md:pb-4">
@@ -103,7 +137,7 @@ export const renderProfile = () => {
 								<div class="flex flex-col">
 									<label for="oldPassword" class="text-base">Old password</label>
 									<div class="flex items-center gap-2">
-									<input class="h-10 rounded px-3 text-black w-11/12" type="password" id="password" name="oldPassword" placeholder="Old password">
+									<input class="h-10 rounded px-3 text-black w-11/12" type="password" id="password" name="oldPassword" placeholder="Old password (required)">
 									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>
 									</div>
 								</div>
