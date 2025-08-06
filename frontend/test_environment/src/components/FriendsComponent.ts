d64c4758165243ed9566a4f3341f156bef94cb2e
P-Claus <pieter.claus1@gmail.com>
2025-08-06 19:30:59 +0200

    started work on showing the friends that are a result of the search query

--- frontend/test_environment/src/components/FriendsComponent.ts
+++ frontend/test_environment/src/components/FriendsComponent.ts
@@ -14,7 +14,7 @@ export const attachFriendsListener= async () => {
 
 		console.log("The friends response is: ", friendsResponse);
 
-		let userArray;
+		let userArray: Array<{ avatar: string, id: number, nickname: string }> = [];
 
     		const container = document.getElementById("dashboard-content");
     		if (container) {
@@ -34,18 +34,25 @@ export const attachFriendsListener= async () => {
     		}
 
    		debounceTimeout = setTimeout(async () => {
-      			try {
-        			const response = await axios.get("https://trans.ella-peeters.me/api/users", {
-          			params: { search: value }, 
-        		});
-        		console.log("Search results:", response.data);
-			userArray = response.data;
-      			} catch (error) {
-        			console.error("Error fetching users:", error);
-      			}
-    		}, 1000);
+			if (value.length >= 3)
+			{
 
-	});
+      				try {
+        				const response = await axios.get(`https://trans.ella-peeters.me/api/users/${userId}/search`, {
+          				params: { q: value }, 
+        			});
+        			console.log("Search results:", response.data);
+				userArray = response.data;
+				const container = document.getElementById("dashboard-content");
+      				if (container) {
+        				container.innerHTML = renderFriends(userArray);
+      				}
+      				} catch (error) {
+        				console.error("Error fetching users:", error);
+      				}}
+    				}, 1000);
+			
+		});
 }
 
   	} catch (error) {
@@ -57,7 +64,7 @@ export const attachFriendsListener= async () => {
   	}
 };
 
-export const renderFriends = (userArray: Array<{id: number, nickname: string}>) => {
+export const renderFriends = (userArray: Array<{avatar: string, id: number, nickname: string}>) => {
 	return `
 				<div class="px-5 flex flex-col md:flex-col flex-1">
 					<div class="px-10 py-5 rounded-xl my-5 mb-10 bg-gray-900 flex flex-col justify-between">
@@ -81,22 +88,36 @@ export const renderFriends = (userArray: Array<{id: number, nickname: string}>) 
 							</div>
 
 						</div>
-						<div class="my-4 py-4 px-8 bg-slate-800 rounded-md border border-slate-700">
+						<div class="my-4 py-4 px-8 bg-slate-800 rounded-md border border-slate-700 min-h-64">
 							<h1 class="text-xl mb-4">Look for players</h1>
 							<input
 								id="searchInput"
   								type="text"
-  								placeholder="Search for a player by typing a nickname..."
+  								placeholder="Search for a player by typing at least 3 characters of a nickname..."
   								class="w-full px-4 py-2 bg-slate-600 text-white placeholder-slate-400 placeholder:text-sm rounded-md border border-slate-700 focus:outline-none focus:ring-2 focus:ring-purple-500"
 							/>
-							<div class="flex flex-col items-center py-12">
-								<p class="text-xl text-gray-400 mb-2 font-extralight">Search for players</p>
-								<p class="text-m text-gray-400 font-extralight">Enter a nickname to find other Pong players</p>
-							</div>
 
 							<div>
-								<p>Here comes the array of found users</p>
+  								${userArray.length > 0 ? `
+    									<div class="grid grid-cols-1 gap-4 mt-4 items-center">
+      										${userArray.map(user => `
+        										<div class="flex bg-slate-600 border border-slate-400 rounded-md py-4 px-8 flex justify-between items-center">
+												<div class="flex flex-row items-center">
+													<img src=${user.avatar || 'https://ui-avatars.com/api/?name=' + encodeURIComponent(user.nickname) + '&background=000000&color=ffffff&bold=true'} class="h-10"></img>
+         												<p class="font-semibold text-white text-m ml-3 truncate max-w-[180px]">${user.nickname}</p>
+												</div>
+												<button>Profile</button>
+        										</div>
+      										`).join('')}
+    									</div>
+  									` : `
+									<div class="flex flex-col items-center py-12">
+										<p class="text-xl text-gray-400 mb-2 font-extralight">Search for players</p>
+										<p class="text-m text-gray-400 font-extralight">Enter at least 3 characters of a nickname to find other Pong players</p>
+									</div>
+  								`}
 							</div>
+
 
 
 						</div>
