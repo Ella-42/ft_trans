4a088a6480fb39b0681811be419c06d9ab286419
Ella <EllaP.jobs@gmail.com>
2025-08-12 04:47:18 +0200

    added graph to visualize score difference over time overlapping win/loss difference to see performance, accidentally coded it confined to per page dataset, will modify it slightly and also make another for data of all time next time(auto push :heart:)

--- frontend/test_environment/src/components/StatsComponent.ts
+++ frontend/test_environment/src/components/StatsComponent.ts
@@ -7,16 +7,101 @@ export const attachStatsListener= async (page = 1) => {
 	try {
 		const idResponse = await axios.get('https://trans.ella-peeters.me/api/whoami');
     		const userId = idResponse.data.id;
-    		const matchResponse = await axios.get(`https://trans.ella-peeters.me/api/users/${userId}/history?page=${page}`);
+			const matchResponse = await axios.get(`https://trans.ella-peeters.me/api/users/${userId}/history?page=${page}&limit=10`);
 		const { totalCount, results: matchHistoryArray, totalPages, currentPage } = matchResponse.data;
     		const winsAndLossesResponse = await axios.get(`https://trans.ella-peeters.me/api/users/${userId}/pong`);
 		const wins = winsAndLossesResponse.data.pong_wins;
 		const losses = winsAndLossesResponse.data.pong_losses;
-    		const container = document.getElementById("dashboard-content");
-    		if (container) {
-      			container.innerHTML = renderStats(userId, totalCount, wins, losses, matchHistoryArray, currentPage, totalPages);
+
+		const scoreData = matchHistoryArray.map(match => {
+			const [score1, score2] = match.info.split('-').map(Number);
+			const winnerScore = score1 > score2 ? score1 : score2;
+			const loserScore = score1 > score2 ? score2 : score1;
+
+			let diff = winnerScore - loserScore;
+			if (match.winner.id !== userId)
+				diff = -diff;
+
+			return { time: match.time, diff };
+		});
+
+		const container = document.getElementById("dashboard-content");
+		if (container) {
+			container.innerHTML = renderStats(userId, totalCount, wins, losses, matchHistoryArray, currentPage, totalPages);
+
+			const canvas = document.getElementById('scoreGraph') as HTMLCanvasElement;
+			const ctx = canvas.getContext('2d');
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+			const total = scoreData.length;
+
+			const diffs = scoreData.map(data => data.diff);
+			const normalizedDiffs = scoreData.map(data => Math.abs(data.diff));
+			const minDiff = Math.min(...normalizedDiffs);
+			const maxDiff = Math.max(...normalizedDiffs);
+
+			const padding = 50;
+			const xScale = (index) => padding + ((total - 1 - index) / (total - 1)) * (canvas.width - padding * 2);
+			const yScale = (diff) => canvas.height - padding - ((Math.abs(diff) - minDiff) / (maxDiff - minDiff)) * (canvas.height - padding * 2);
+
+			ctx.strokeStyle = '#16a34a';
+			ctx.beginPath();
+			scoreData.forEach((point, i) => {
+				if (point.diff > 0) {
+					const x = xScale(i);
+					const y = yScale(point.diff);
+					if (i === 0) ctx.moveTo(x, y);
+					else ctx.lineTo(x, y)
+				}
+			});
+			ctx.stroke();
+
+			ctx.strokeStyle = 'red';
+			ctx.beginPath();
+			scoreData.forEach((point, i) => {
+				if (point.diff < 0) {
+					const x = xScale(i);
+					const y = yScale(point.diff);
+					if (i === 0) ctx.moveTo(x, y);
+					else ctx.lineTo(x, y)
+				}
+			});
+			ctx.stroke();
+
+			const tickCount = 9;
+			const tickLength = 5;
+			ctx.fillStyle = 'white';
+			ctx.strokeStyle = 'white';
+			ctx.font = '12px sans-serif';
+
+			for (let i = 0; i <= tickCount; i++) {
+				const matchIndex = Math.floor(i * (total - 1) / tickCount);
+				const x = xScale(matchIndex);
+				const date = new Date(scoreData[matchIndex].time);
+				const label = date.toLocaleDateString('nl-BE', { month: 'short', day: 'numeric' });
+
+				ctx.beginPath();
+				ctx.moveTo(x, canvas.height - padding);
+				ctx.lineTo(x, canvas.height - padding + tickLength);
+				ctx.stroke();
+
+				ctx.fillText(label, x - 15, canvas.height - padding + tickLength + 20);
+			}
+
+			for (let i = 0; i <= tickCount; i++) {
+				const diffVal = minDiff + ((maxDiff - minDiff) / tickCount) * i;
+				const y = yScale(diffVal);
+
+				ctx.beginPath();
+				ctx.moveTo(padding - tickLength, y);
+				ctx.lineTo(padding, y);
+				ctx.stroke();
+
+				ctx.fillText(diffVal.toFixed(1), 10, y + 4);
+			}
+
 			attachPaginationListeners(currentPage, totalPages);
-    		}
+		}
   	} catch (error) {
     		console.error("The error is: ", error);
     		const container = document.getElementById("dashboard-content");
@@ -48,7 +133,7 @@ const attachPaginationListeners = (currentPage: number, totalPages: number) => {
 };
 
 
-export const renderStats = (userId: number, totalCount: number, wins: number, losses: number, matchHistoryArray: Array<{time: string, winner: { id: number; nickname: string }, loser: {id: number; nickname: string}}>, currentPage: number, totalPages: number) => {
+export const renderStats = (userId: number, totalCount: number, wins: number, losses: number, matchHistoryArray: Array<{time: string, winner: { id: number; nickname: string }, loser: {id: number; nickname: string}, info: string}>, currentPage: number, totalPages: number) => {
 	return `
 				<div class="px-5 flex flex-col md:flex-col flex-1">
 					<div class="px-10 py-5 rounded-xl my-5 mb-10 bg-gray-900 flex flex-col justify-between">
@@ -56,7 +141,17 @@ export const renderStats = (userId: number, totalCount: number, wins: number, lo
 							<h1 class="text-3xl font-bold mb-2 bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">Statistics</h1>
 							<p class="text-slate-400">Track your Pong performance and match history</p>
 						</div>
-						<div class="grid grid-cols-1 md:grid-cols-3 mt-6 gap-4">
+						<div class="flex flex-col items-center mt-6 text-center">
+							<h2 class="text-lg font-semibold mb-2">Score difference over time</h2>
+							<div class="flex space-x-4">
+								<div class="w-3 h-1 mt-2 bg-green-600 rounded-sm"></div>
+								<span class="text-sm"> Wins</span>
+								<div class="w-3 h-1 mt-2 bg-red-600 rounded-sm"></div>
+								<span class="text-sm"> Losses</span>
+							</div>
+							<canvas id="scoreGraph" width="800" height="400"></canvas>
+						</div>
+						<div class="grid grid-cols-1 md:grid-cols-4 mt-6 gap-4">
 							<div class="py-4 px-8 bg-slate-800 rounded-md">
 								<h2 class="text-sm font-medium text-slate-300">Total matches</h2>
 								<p class="text-2xl font-bold text-white">${totalCount}</p>
@@ -72,11 +167,14 @@ export const renderStats = (userId: number, totalCount: number, wins: number, lo
 								<p class="text-2xl font-bold text-white">${losses}</p>
 								<p class="text-xs text-slate-400">Losses</p>
 							</div>
-
+							<div class="py-6 px-8 bg-slate-800 rounded-md">
+								<h2 class="text-sm font-medium text-slate-300">Win/loss ratio</h2>
+								<p class="text-2xl font-bold text-white">${losses ? (wins / losses).toFixed(2) : '∞'}</p>
+							</div>
 						</div>
 						<div class="grid grid-cols-1 mt-6 gap-4">
 							<div class="py-4 px-8 bg-slate-800 rounded-md">
-								<h2 class="text-sm font-medium text-slate-300 mb-4">Recent matches</h2>
+								<h2 class="text-sm font-medium text-slate-300 mb-4">Match history</h2>
 								${matchHistoryArray.length > 0 ? `
 								<div class="grid grid-cols-1 gap-4">
 									${matchHistoryArray.map(match => {
@@ -95,7 +193,7 @@ export const renderStats = (userId: number, totalCount: number, wins: number, lo
       									<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4 bg-slate-600 rounded-md py-4 px-8 items-center">
 										<p class="${userId === match.winner.id ? 'bg-green-500/20 p-2 text-green-400 w-min h-min rounded-md text-sm border border-green-500/30' : 'bg-red-500/20 p-2 text-red-400 w-min h-min rounded-md text-sm border border-red-500/30'}">${userId === match.winner.id ? "WIN": "LOSS"}</p>
 										<p class="text-sm text-white">vs. ${userId === match.winner.id ? match.winner.nickname : match.loser.nickname}</p>
-        									<p class="text-xs text-slate-400">Score: 4-12</p>
+											<p class="text-xs text-slate-400">Score: ${match.info || '?-?'}</p>
         									<p class="text-xs text-slate-400">${formattedTime}</p>
       									</div>
    								 	`;
